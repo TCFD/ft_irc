11300350013a8f53d45396b957d3a3b5a9a92649
TCFD <r.ciaze@gmail.com>
2024-05-28 22:30:52 +0200

    echo serv'

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <cstring>
 #include <unistd.h>
+#include <stdlib.h>
 #include <poll.h>
 #include <iostream>
 #include <sys/socket.h>
@@ -7,11 +8,20 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 
-typedef struct User 
+char *substr(char *str, int start, int end)
 {
-	char name[256];
-	int number;
-}User;
+	char *buf;
+	buf = (char *)malloc(end - start + 1);
+	int j = 0;
+	if (start > end)
+		return NULL;
+	while (str[start] && start < end) {
+		buf[j] = str[start]; start++; j++;
+	}
+	buf[j] = 0;
+	std::cout << buf << std::endl;
+	return buf;
+}
 
 int main(void) {
 	int	socketServer = socket(AF_INET, SOCK_STREAM, 0);
@@ -27,12 +37,15 @@ int main(void) {
 
 	int	socketClient = accept(socketServer, NULL, NULL);
 	std::cout << "Server says : client accepted\n";
+
 	char test[256];
 
+	memset(test, 256, 0);
 	int bytes = recv(socketClient, test, 256, 0);
 	while (bytes != 0 && bytes != -1) {
-		std::cout << "Server says : We recieved " << test << std::endl;
+		std::cout << "Server says : We recieved " << substr(test, 0, bytes) << std::endl;
 		send(socketClient, test, bytes, 0);
+		memset(test, 256, 0); 	
 		bytes = recv(socketClient, test, 256, 0);
 	}
 
