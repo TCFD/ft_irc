5b31acb5b4f5e1b0c82cbd9e1a835ae0817a468e
Asalic <shiro.nagasaki91@gmail.com>
2024-05-30 12:02:17 +0200

    Set up branche commune

---
+++ Irc.hpp
@@ -0,0 +1,18 @@
+#ifndef IRC_HPP
+# define IRC_HPP
+
+# include <cstdlib>
+# include <cstdio>
+# include <fcntl.h>
+# include <poll.h>
+# include <sys/types.h>
+# include <sys/socket.h>
+# include <netinet/in.h>
+# include <arpa/inet.h>
+# include <iostream>
+# include <cstring>
+# include <unistd.h>
+# include <map>
+# include <vector>
+
+#endif
